2219ec2ba83aec7ed7f3c64cc8c7436ad076ec0c
S <S@mail.minecraft.jp.net>
2015-10-17 18:42:37 +0900

    Update: ポインタで渡すように+コマンドライン引数を受けるように

--- core.go
+++ core.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 // nがmで割り切れるかどうか
-func check(n int, m int) bool {
-	return n % m == 0
+func check(n *uint64, m *uint64) bool {
+	return *n % *m == 0
 }
 
-// nがa(スライスのポインタ)で割り切れるかどうか
-func checkAll(n int, a *[]int) bool {
+// nがa(スライス)で割り切れるかどうか
+func checkAll(n *uint64, a *[]uint64) bool {
 	arr := *a
-	ok := false
+	ok := true
 	for _, i := range arr{
 		fmt.Println(i)
-		if check(n, i){
-			ok = true
+		if check(n, &i){
+			ok = false
 			break
 		}
 	}
@@ -24,8 +26,18 @@ func checkAll(n int, a *[]int) bool {
 }
 
 func main() {
-	arr := []int{2, 3, 5, 7, 9}
-	fmt.Println(checkAll(8, &arr))
+	// 調べる数(uint)
+	checkNum, err := strconv.ParseUint(os.Args[0], 10, 64)
+	if err != nil {
+		fmt.Println("ParseError: 引数のパースに失敗しました")
+		fmt.Println(err)
+		fmt.Println()
+		checkNum = 13
+	}
+	fmt.Println("引数: " + strconv.FormatUint(checkNum, 10) + "\n")
+
+	arr := []uint64{2, 3, 5, 7, 9}
+	fmt.Println(checkAll(&checkNum, &arr))
 }
 
 
